3cc7cdb5aaf17ac4e1ff0b96665bb788eae14aa7
Joshua Feliciano <41080599+joshfeli@users.noreply.github.com>
2025-05-16 02:33:04 -0400

    Add basic HTML to Contact panel (still working on JS animation)

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 let activePanel
+
 // make the home panel visible; all others invisible by default
 window.addEventListener("load", event => {
     document.querySelector("nav").classList.remove("unloaded")
     makeVisible("home")
-    activePanel = "home"
 })
 
 const buttons = document.querySelectorAll("nav > button")
@@ -11,12 +11,10 @@ for (let button of buttons) {
     button.addEventListener("click", event => {
         // switch visibility from active panel to
         // the panel `button` represents
-        let panelName = button.innerHTML.toLowerCase()
 
+        let panelName = button.innerHTML.toLowerCase()
         makeInvisible(activePanel)
         makeVisible(panelName)
-
-        activePanel = panelName
     })
 }
 
@@ -34,21 +32,23 @@ function makeVisible(panelName){
 
     panelContent.classList.add("active")
     panelButton.classList.add("active")
-    // panelContent.style.display = "flex"
-    // panelButton.style.display = "none"
+
+    // set the activePanel to be this panel
+    activePanel = panelName
 }
 
 function makeInvisible(panelName){
     /**
      * Reverse the effects of `makeVisible`, i.e.,
-     * make a panel _invisible_ by settin its content's display to `none`
+     * make a panel _invisible_ by setting its content's display to `none`
      * and revealing its button.
+     * 
+     * @param {string} panelName - A string representing a panel name, corresponding to an HTML class
+     * (e.g., "home", "skills", etc)
      */
     const panelContent = document.querySelector(`main.${panelName}`)
     const panelButton = document.querySelector(`button.${panelName}`)
 
     panelContent.classList.remove("active")
     panelButton.classList.remove("active")
-    // panelContent.style.removeProperty("display") // reset to default of "none"
-    // panelButton.style.removeProperty("display") // reset to default of visible (in a flex display)
 }
